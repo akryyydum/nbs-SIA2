571142f7a391c8fca42c5a81ee937ec4de8755ae
akryyydum <lanceandres12.la@gmail.com>
2025-07-01 09:32:31 +0800

    order received fix

--- server/routes/orderRoutes.js
+++ server/routes/orderRoutes.js
@@ -7,11 +7,13 @@ const {
   getAllOrders,
   getOrderById,
   deleteOrder,
-  acceptOrder,    // add this
-  declineOrder,  
-  shipOrder,  // add this
-  getOrderMetrics
+  acceptOrder,
+  declineOrder,
+  shipOrder,
+  getOrderMetrics,
+  markOrderReceived
 } = require('../controllers/orderController');
+
 // User: Create order
 router.post('/', protect, createOrder);
 
@@ -21,18 +23,21 @@ router.get('/my', protect, getMyOrders);
 // Admin: Get all orders
 router.get('/', protect, sales, getAllOrders);
 
-// Admin or owner: Get order by ID
-router.get('/:id', protect, getOrderById);
-
-// Admin: Delete order
-router.delete('/:id', protect, sales, deleteOrder);
-
 // Accept order
 router.put('/:id/accept', protect, sales, acceptOrder);
 router.put('/:id/ship', protect, admin, shipOrder);
 
 // Decline order
 router.put('/:id/decline', protect, sales, declineOrder);
+
+// Mark as received
+router.put('/:id/received', protect, markOrderReceived);
+
+// Admin or owner: Get order by ID
+router.get('/:id', protect, getOrderById);
+
+// Admin: Delete order
+router.delete('/:id', protect, sales, deleteOrder);
 
 // Get order visuals
 // router.get('/visuals', protect, sales, getOrderVisuals);
